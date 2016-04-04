d98dea285eceea8b169e19869d9cd75976fa2c5c
Logan Marshall <wildmovementco@gmail.com>
2016-04-03 21:49:12 -0400

    set up project and completed first problem set

---
+++ ps0.py
@@ -0,0 +1,7 @@
+# Problem Set 0
+# Name: Logan Marshall
+# Time Spent: 2:30
+
+dateOfBirth = raw_input('Enter your date of birth')
+lastName = raw_input('Enter your last name')
+print(lastName + " " + dateOfBirth)
